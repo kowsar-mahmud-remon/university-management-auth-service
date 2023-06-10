d4cdc5ab4b99474e8471898b81e43bad0f253ded
creativerze <creativerze@gmail.com>
2023-06-10 13:00:52 +0600

    optimized code

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { UserRoutes } from './app/modules/user/user.route';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import routes from './app/routes';
 
 const app: Application = express();
 
@@ -13,14 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // application routes
-// console.log(process.env)
-app.use('/api/v1/users/', UserRoutes);
-app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
-
-// testing
-// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-//   console.log(x)
-// })
+app.use('/api/v1/', routes);
 
 app.use(globalErrorHandler);
 
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,3 +1,6 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
@@ -5,6 +8,10 @@ const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
   const result = await AcademicSemester.create(payload);
+
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester code');
+  }
   return result;
 };
 
